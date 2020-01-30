c676f83a21d0e36836da12edfed66003fdcef2b5
MrUbaidUllahZahid <59232179+MrUbaidUllahZahid@users.noreply.github.com>
2020-01-30 22:27:02 +0500

    Mosh with python tutorial

---
+++ Build a game.py
@@ -0,0 +1,19 @@
+command = ""
+while True:
+     command = input("> ")
+     if command == "start":
+         print("Car started...")
+     elif command == "stop":
+          print("car stopped...")
+     elif command == "help":
+         print("""      
+    start - to start the car
+    stop - to stop the car
+    quit - to quit
+    """)
+     elif command == "quit":
+        break
+     else:
+        print("Sorry, I don't understand that!")
+
+    
