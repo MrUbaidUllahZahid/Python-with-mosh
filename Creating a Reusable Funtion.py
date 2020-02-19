b25acf6b419288e9b30788a4709c2b8c5befdcf1
MrUbaidUllahZahid <59232179+MrUbaidUllahZahid@users.noreply.github.com>
2020-02-19 22:50:06 +0500

    Mosh with python

--- Creating a Reusable Funtion.py
+++ Creating a Reusable Funtion.py
@@ -1,10 +1,14 @@
+def emoji_converter(message):
+    words = message.split(" ")
+    emojis = {
+        ":)": "😊",
+        ":(": "😌"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+
 message = input(">")
-words = message.split(" ")
-emojis = {
-    ":)": "😊",
-    ":(": "😌"
-}
-output = ""
-for word in words:
-    output += emojis.get(word, word) + " "
-print(output)
+print(emoji_converter(message))
--- closer look at list.py
+++ closer look at list.py
@@ -1,3 +1,4 @@
 names = ('Ubaid', 'Ullah', 'Zahid', 'Iqbal')
+names [0] = 'Jhon'
 print(names[2:])
 print(names)
